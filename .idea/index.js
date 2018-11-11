34354d28f787ae5d7221354646e2c7ce4b27ca24
Anders Feyling <feylinganders@gmail.com>
2018-11-11 19:10:26 +0100

    records when multiple users connect

--- .idea/index.js
+++ .idea/index.js
@@ -1,10 +1,15 @@
-const app = require(`express`)();
-const http = require(`http`).Server(app);
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 
 app.get(`/`, function (req, res) {
-    res.sendFile(__dirname + `/index.html`);
+    res.sendFile(__dirname + "/index.html");
 })
 
 http.listen(3000, function () {
     console.log("Listening on *:3000");
+})
+
+io.on("connection", function (socket)  {
+    console.log("A user connected");
 })
