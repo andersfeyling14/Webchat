76acb84098f02adc8fb7172d90dd56d27d8795e8
Anders Feyling <feylinganders@gmail.com>
2018-11-11 20:15:29 +0100

    chat messages show up on screen

--- .idea/index.js
+++ .idea/index.js
@@ -10,6 +10,23 @@ http.listen(3000, function () {
     console.log("Listening on *:3000");
 })
 
-io.on("connection", function (socket)  {
+io.on("connection", function (socket) {
     console.log("A user connected");
+    socket.on("disconnect", function () {
+        console.log("User disconnected");
+    })
 })
+
+io.on("connection", function (socket) {
+    socket.on("chat message", function (msg) {
+        console.log("message: ", msg);
+    });
+});
+
+io.emit("some event", {for: "everyone"});
+
+io.on("connection", function (socket) {
+   socket.on("chat message", function (msg) {
+       io.emit("chat message", msg);
+   });
+});
